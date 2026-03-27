009380eaabd4039d423b6c9273eda3c13977bd01
aocampo558 <aocampo2019@sdsu.edu>
2026-03-26 22:47:25 -0700

    changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 // ALL YOU JS GOES HERE
+
+const hamburger = document.querySelector('.menu-toggle');
+const dropdown = document.querySelector('.dropdown').classList;
+
+
+function revealDropdown() {
+    dropdown.toggle('dropdown')
+};
+
+hamburger.addEventListener('click', revealDropdown);
+
+
+
+
+const navkinks = document.querySelectorAll('a.links')
+for (let i = 0; i < navlinks.length; i ++) 
+    
+{
+navlinks [i].addEventListener('click', closemenu)
+}
+
+function closemenu () {
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.classList.remove('show');
+}
