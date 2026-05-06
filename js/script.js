e0b657f84bfb770f0340d813add6b4fff2614948
aocampo558 <aocampo2019@sdsu.edu>
2026-05-05 20:00:50 -0700

    New updates

--- js/script.js
+++ js/script.js
@@ -42,3 +42,15 @@ if (today.getHours() >= 18 || today.getHours() <6) {
   document.getElementById("lightmode").setAttribute("href", "css/styles.css");
 }
 
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (top < windowHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+});
